683c8eab4439f42b358f9fadd24757a5d3cd2c1d
luna <eduardaluna22@gmail.com>
2020-09-20 20:36:54 -0300

    adicionando views para o que foi criado com seeders

--- site-admin-pre-vest/routes/web.php
+++ site-admin-pre-vest/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/listar/administradores', "AdministradorController@listar");
+
+Route::get('/listar/alunos', "AlunoController@listar");
